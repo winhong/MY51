6ffdf3d0f4347117ea753b11a5911dd9258d5fb8
winhong <huanghuangmaojian@huangmaojiandeiMac.local>
2015-01-05 16:31:30 +0800

    新建项目MY51

---
+++ MY51/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MY51
+//
+//  Created by 黄茂坚 on 15/1/5.
+//  Copyright (c) 2015年 黄茂坚. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ MY51/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MY51
+//
+//  Created by 黄茂坚 on 15/1/5.
+//  Copyright (c) 2015年 黄茂坚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
