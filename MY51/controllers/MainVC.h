c129aa77c19e645eefe6b391a772405d6270809d
winhong <huanghuangmaojian@huangmaojiandeiMac.local>
2015-01-05 16:39:16 +0800

    新增main文件

---
+++ MY51/controllers/MainVC.h
@@ -0,0 +1,13 @@
+//
+//  MainVC.h
+//  buyMedicine
+//
+//  Created by chenjie on 15/1/4.
+//  Copyright (c) 2015年 winhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainVC : UIViewController
+
+@end
